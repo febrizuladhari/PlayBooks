cca46f82b96bc66cdce1c6ada9b0eb16a6e39955
Your Name <you@example.com>
2022-05-19 22:42:18 +0700

    DONNY NEW

--- TemplateBelumJadi/bukuSaya.php
+++ TemplateBelumJadi/bukuSaya.php
@@ -59,35 +59,9 @@
                         <form class="form-inline">
                             <input class="form-control mr-sm-2" style="width: 700px;" type="search" placeholder="Telusuri" aria-label="Search">
                             <button class="btn btn-info my-2 my-sm-0" type="submit"><i class="fa fa-search fa-lg"></i></button>
-                            <button class="btn btn-info mx-2 my-2 my-sm-0" type="button" data-toggle="modal" data-target="#exampleModal">Upload File</button>
+                            
+                    
                         </form>
-                    </div>
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalLabel">New message</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <form>
-                                                        <div class="form-group">
-                                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                                            <input type="text" class="form-control" id="recipient-name">
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <label for="message-text" class="col-form-label">Message:</label>
-                                                            <textarea class="form-control" id="message-text"></textarea>
-                                                        </div>
-                                                    </form>
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <button type="button" class="btn btn-primary">Send message</button>
-                                                </div>
-                                            </div>
-                                        </div>
                     </div>
                     <?php
                     
@@ -193,6 +167,55 @@
                                             <li class="nav-item ">
                                                 <a class="nav-link"  href="#"><i class="fa fa-cog fa-lg"></i></a>
                                             </li>
+                                            <button class="btn btn-info mx-2 my-2 my-sm-0" type="button" data-toggle="modal" data-target="#exampleModal">Upload File</button>                                       
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Upload File</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                        <form method="POST" enctype="multipart/form-data">
+                                                            <div class="form-group">
+                                                                <label for="recipient-name" class="col-form-label">ID FILE :</label>
+                                                                <input type="text" class="form-control" id="recipient-name" name="id">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <label for="message-text" class="col-form-label">Input File:</label>
+                                                                <div class="input-group ">
+                                                                    <input type="file" class="form-control" style="height:auto" id="inputGroupFile01" name="namafile"> 
+                                                                </div>
+                                                            </div>
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                            <button type="submit" class="btn btn-primary" name="buttonup">Upload File</button>
+                                                            <?php
+                                                                require_once'../SourceCode/includes/koneksi.php';
+                                                                
+                                                                
+                                                                if(isset($_POST['buttonup'])){
+                                                                    $id_file = $_POST['id'];
+                                                                    $direktori = "../TemplateBelumJadi/file/";
+                                                                    $file_name=$_FILES['namafile']['name'];
+                                                                    move_uploaded_file($_FILES['namafile']['tmp_name'],$direktori.$file_name);
+                                                                    $sql = "INSERT INTO file (id_file, contain) VALUES ('$id_file','$file_name')";
+                                                                                        
+                                                                    if($koneksi->query($sql)===TRUE){
+                                                                    echo "<script>setTimeout(\"location.href = 'bukuSaya.php';\",1500);</script>";
+                                                                    } else {
+                                                                    echo "Terjadi kesalahan:".$sql."<br/>".$koneksi->error;
+                                                                    }
+
+                                                                    
+                                                                }
+                                                            ?>
+                                                        </form>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </ul>
                                     </div>
                                 </div>
